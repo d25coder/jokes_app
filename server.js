970431af1f8e9f220311e8dde442c508021c1111
d25coder <jamerria15@yahoo.com>
2025-10-27 12:43:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Step 1 Import Express and Create Port
+const express = require('express')
+const server = express()
+const router = require('./routes/router')
+const PORT = process.env.PORT || 3001
+
+
+server.use('/',router)
+server.set('view engine','ejs')
+
+
+// Step 2 listen...
+server.listen(PORT,()=> console.log(`Server is listening at http://localhost:${PORT}`))
